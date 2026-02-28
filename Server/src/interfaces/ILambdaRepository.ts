c30528d89b10b70e8073e50a2a644a377933bb90
Shovan04 <shovanm50@gmail.com>
2026-02-28 19:48:48 +0530

    feat(intelligence): add ResourceIntelligenceService for AWS resource evaluation

--- Server/src/interfaces/ILambdaRepository.ts
+++ Server/src/interfaces/ILambdaRepository.ts
@@ -1,7 +1,11 @@
+import { ResourceIntelligence } from "../services/ResourceIntelligenceService.js";
+
 export interface LambdaFunction {
   functionName: string;
   runtime: string;
   lastModified: string;
+  memorySize?: number;
+  intelligence?: ResourceIntelligence;
 }
 
 export interface ILambdaRepository {
